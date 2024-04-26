8a4ac6315e2b44b6d5d7206b2903ca73442d0a63
Daniel_PC <galvaomartinsdaniel@gmail.com>
2024-04-26 01:39:12 -0300

    Feito teste dos modelos de erros tudo certo

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,7 +1,14 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/Dankei/meu-primeiro-crud-go.git/src/configuration/rest_err"
+	"github.com/gin-gonic/gin"
+)
 
 func CreateUser(c *gin.Context){
-	
+
+	err := rest_err.NewBadRequestError("Você erroou")
+	c.JSON(err.Code,err)
+
+
 }
