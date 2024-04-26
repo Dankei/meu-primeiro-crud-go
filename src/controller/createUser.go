c7a204074aa6da1e48ffde1745dea7fc0a7c1bdd
Daniel_PC <galvaomartinsdaniel@gmail.com>
2024-04-26 11:17:47 -0300

    Adicionado Personalização de erros de validação e retorno das causas

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -2,8 +2,9 @@ package controller
 
 import (
 	"fmt"
+	"log"
 
-	"github.com/Dankei/meu-primeiro-crud-go.git/src/configuration/rest_err"
+	"github.com/Dankei/meu-primeiro-crud-go.git/src/configuration/validation"
 	"github.com/Dankei/meu-primeiro-crud-go.git/src/controller/model/request"
 	"github.com/gin-gonic/gin"
 )
@@ -13,10 +14,9 @@ func CreateUser(c *gin.Context){
 	var UserRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&UserRequest); err != nil{
-		restErr := rest_err.NewBadRequestError(
-			fmt.Sprintf("There are some incorrect filds, error=%s",err.Error()))
-
-		c.JSON(restErr.Code,restErr)
+		log.Printf("Error trying to marshal object, error=%s\n",err.Error())
+		errRest := validation.ValidateUserError(err)
+		c.JSON(errRest.Code,errRest)
 		return
 	}
 
